a770cc49593f1f6818e5436336d0408b08be4ee9
Mira090 <2935shu4182@gmail.com>
2025-12-11 00:37:16 +0900

    ダッシュ攻撃範囲拡大の古いコードを削除

--- SephiriaMod/Items/Charm_DashAttackScaleUp.cs
+++ SephiriaMod/Items/Charm_DashAttackScaleUp.cs
@@ -13,7 +13,6 @@ namespace SephiriaMod.Items
     public class Charm_DashAttackScaleUp : Charm_StatusInstance
     {
         public int[] rangeByLevel = [30, 40, 50, 60, 70];
-        private bool effect = false;
         public override Loc.KeywordValue[] BuildKeywords(UnitAvatar avatar, int level, int virtualLevelOffset, bool showAllLevel, bool ignoreAvatarStatus)
         {
             string value = showAllLevel ? rangeByLevel.SafeRandomAccess(0) + "→" + rangeByLevel.SafeRandomAccess(maxLevel) : rangeByLevel.SafeRandomAccess(LevelToIdx(level)).ToString();
@@ -25,62 +24,17 @@ namespace SephiriaMod.Items
         protected override void OnEnabledEffect()
         {
             base.OnEnabledEffect();
-            //Events.OnPreBasicAttack += OnPreAttack;
-            //Events.OnPreSpecialAttack += OnPreAttack;
-            //WeaponController.OnBaisAttackSwing += OnAttackSwing;
-            //WeaponController.OnSpecialAttackSwing += OnAttackSwing;
-            if (!effect)
-            {
-                //NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
-                effect = true;
-            }
             NetworkAvatar.AddCustomStat("DashAttackRange", rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
         }
 
-        private void OnPreAttack(WeaponControllerSimple weapon, UnitAvatar avatar)
-        {
-            if (avatar != NetworkAvatar)
-                return;
-            //Melon<Core>.Logger.Msg("OnPreAttack: " + effect);
-            if (effect)
-            {
-                effect = false;
-                NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, -rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
-            }
-        }
-
-        private void OnAttackSwing(int idx)
-        {
-            //Melon<Core>.Logger.Msg("OnAttackSwing1: " + effect);
-            if (!effect)
-            {
-                //NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
-                effect = true;
-            }
-            //Melon<Core>.Logger.Msg("OnAttackSwing2: " + effect);
-        }
         protected override void OnDisabledEffect()
         {
             base.OnDisabledEffect();
-            //Events.OnPreBasicAttack -= OnPreAttack;
-            //Events.OnPreSpecialAttack -= OnPreAttack;
-            //WeaponController.OnBaisAttackSwing -= OnAttackSwing;
-            //WeaponController.OnSpecialAttackSwing -= OnAttackSwing;
-            if (effect)
-            {
-                effect = false;
-                //NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, -rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
-            }
             NetworkAvatar.AddCustomStat("DashAttackRange", -rangeByLevel.SafeRandomAccess(CurrentLevelToIdx()));
         }
         protected override void OnUpdatedLevel(int oldLevel, int newLevel)
         {
             base.OnUpdatedLevel(oldLevel, newLevel);
-            if (effect)
-            {
-                //NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, -rangeByLevel.SafeRandomAccess(LevelToIdx(oldLevel)));
-                //NetworkAvatar.AddCustomStat(ECustomStat.WeaponRange, rangeByLevel.SafeRandomAccess(LevelToIdx(newLevel)));
-            }
             NetworkAvatar.AddCustomStat("DashAttackRange", -rangeByLevel.SafeRandomAccess(LevelToIdx(oldLevel)));
             NetworkAvatar.AddCustomStat("DashAttackRange", rangeByLevel.SafeRandomAccess(LevelToIdx(newLevel)));
         }
