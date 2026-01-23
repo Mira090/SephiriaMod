267d3b75e4c840677c322da5e56fb789e733d7ac
Mira090 <2935shu4182@gmail.com>
2026-01-23 19:58:44 +0900

    コンボ（アイテムカテゴリー）一覧に元素、錬金術、鍛冶場を追加し、コメントを追加

--- SephiriaMod/Utilities/ItemCategories.cs
+++ SephiriaMod/Utilities/ItemCategories.cs
@@ -18,31 +18,106 @@ namespace SephiriaMod.Utilities
                 //Melon<Core>.Logger.Msg("Categories: " + pro.Name.ToSephiriaUpperId());
             }
         }
-        public static string Charging { get; private set; }
+        /// <summary>
+        /// 仲間
+        /// </summary>
         public static string Companion { get; private set; }
+        /// <summary>
+        /// 雷雲
+        /// </summary>
         public static string DarkCloud { get; private set; }
+        /// <summary>
+        /// 燠火
+        /// </summary>
         public static string Ember { get; private set; }
-        public static string Favorites { get; private set; }
+        /// <summary>
+        /// 太陽剣
+        /// </summary>
         public static string FlameSword { get; private set; }
+        /// <summary>
+        /// 氷の武具
+        /// </summary>
         public static string Frost { get; private set; }
+        /// <summary>
+        /// 氷河
+        /// </summary>
         public static string Glacier { get; private set; }
+        /// <summary>
+        /// 守護
+        /// </summary>
         public static string Guardian { get; private set; }
+        /// <summary>
+        /// 魔法工学
+        /// </summary>
         public static string Magitech { get; private set; }
+        /// <summary>
+        /// 神秘
+        /// </summary>
         public static string Mystic { get; private set; }
-        public static string None { get; private set; }
+        /// <summary>
+        /// 惑星
+        /// </summary>
         public static string Planet { get; private set; }
+        /// <summary>
+        /// 精密
+        /// </summary>
         public static string Precision { get; private set; }
+        /// <summary>
+        /// 影
+        /// </summary>
         public static string Shadow { get; private set; }
+        /// <summary>
+        /// 堅固
+        /// </summary>
         public static string Sturdy { get; private set; }
+        /// <summary>
+        /// 風の歌
+        /// </summary>
         public static string WindSong { get; private set; }
+        /// <summary>
+        /// アカデミー
+        /// </summary>
         public static string Academy { get; private set; }
+        /// <summary>
+        /// 湖
+        /// </summary>
         public static string Lake { get; private set; }
+        /// <summary>
+        /// 呪い
+        /// </summary>
         public static string Curse { get; private set; }
+        /// <summary>
+        /// 交渉
+        /// </summary>
         public static string Savvy { get; private set; }
+        /// <summary>
+        /// 元素
+        /// </summary>
+        public static string Elemental { get; private set; }
+        /// <summary>
+        /// 錬金術
+        /// </summary>
+        public static string Alchemy { get; private set; }
+        /// <summary>
+        /// 鍛冶場
+        /// </summary>
+        public static string Weapon { get; private set; }
         //public static string Purgatory { get; private set; }
+        /// <summary>
+        /// 生命
+        /// </summary>
         public static string Vitality { get; private set; }
+        /// <summary>
+        /// 夜空
+        /// </summary>
         public static string Stargaze { get; private set; }
+        /// <summary>
+        /// 空の歌
+        /// </summary>
         public static string SkySong { get; private set; }
+        /// <summary>
+        /// 酩酊
+        /// </summary>
         public static string Drunk { get; private set; }
     }
 }
