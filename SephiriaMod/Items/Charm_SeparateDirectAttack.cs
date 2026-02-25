284910ec2d7f2e434aa2fef163dbdbcf01e8a8de
Mira090 <2935shu4182@gmail.com>
2026-02-26 01:38:25 +0900

    惜別のアレキサンドライトにクールダウンを追加（0.1秒あたり最大5回まで分割）

--- SephiriaMod/Items/Charm_SeparateDirectAttack.cs
+++ SephiriaMod/Items/Charm_SeparateDirectAttack.cs
@@ -8,6 +8,10 @@ namespace SephiriaMod.Items
     public class Charm_SeparateDirectAttack : Charm_StatusInstance
     {
         public int[] separate = [56, 52, 48, 42, 36];
+        public Timer cooldown = new Timer(0.1f);
+        public int cooldownCount = 0;
+        public int countMax = 5;
+        public bool IsInCooldown => cooldownCount > countMax;
         public override Loc.KeywordValue[] BuildKeywords(UnitAvatar avatar, int level, int virtualLevelOffset, bool showAllLevel, bool ignoreAvatarStatus)
         {
             string value = showAllLevel ? separate.SafeRandomAccess(0) + "→" + separate.SafeRandomAccess(maxLevel) : separate.SafeRandomAccess(LevelToIdx(level)).ToString();
@@ -21,14 +25,25 @@ namespace SephiriaMod.Items
             base.OnEnabledEffect();
             NetworkAvatar.OnAttackUnitBeforeOperation += OnAttackUnitBeforeOperation;
         }
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+            if(cooldownCount > 0 && cooldown.Update(Time.deltaTime))
+            {
+                cooldownCount = 0;
+            }
+        }
 
         private void OnAttackUnitBeforeOperation(UnitAvatar avatar, DamageInstance damage)
         {
+            if (IsInCooldown)
+                return;
             if (damage.id == "Charm_MinHPKill")//白い卵の殻は除外
                 return;
             if(damage.fromType == EDamageFromType.DirectAttack && damage.damage >= separate.SafeRandomAccess(CurrentLevelToIdx()))
             {
                 damage.damage -= damage.damage / 2f;
+                cooldownCount++;
                 var color = damage.useCustomColor;
                 if (!color)
                     damage.SetCustomColor(false, new Color(0.25f, 0.25f, 0.8f));
