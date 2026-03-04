d321e1b239d6b260649fc8c8d33a25b201424007
Mira090 <2935shu4182@gmail.com>
2026-03-04 22:12:46 +0900

    新しい氷河アーティファクトの準備

---
+++ SephiriaMod/Items/Charm_GuardFrostbite.cs
@@ -0,0 +1,37 @@
+﻿using SephiriaMod.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SephiriaMod.Items
+{
+    public class Charm_GuardFrostbite : Charm_StatusInstance
+    {
+        protected override void OnEnabledEffect()
+        {
+            base.OnEnabledEffect();
+            NetworkAvatar.OnGuardSucceeded += OnGuardSucceeded;
+        }
+
+        private void OnGuardSucceeded(DamageInstance damage, bool perfect)
+        {
+            if(damage.origin is UnitAvatar attacker && !attacker.IsDead && SephiriaPrefabs.Frostbite is CharacterDebuff_Frostbite frostbite)
+            {
+                if (perfect)
+                {
+                    attacker.ApplyDebuff(frostbite.freezeDebuffPrefab, NetworkAvatar);
+                }
+                else
+                {
+                    attacker.ApplyDebuff(frostbite, NetworkAvatar);
+                }
+            }
+        }
+
+        protected override void OnDisabledEffect()
+        {
+            base.OnDisabledEffect();
+            NetworkAvatar.OnGuardSucceeded -= OnGuardSucceeded;
+        }
+    }
+}
