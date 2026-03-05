d048a6b3c3c8dd5a745d0056df6b46257af7060f
Mira090 <2935shu4182@gmail.com>
2026-03-05 19:25:57 +0900

    DamageIdの調整2

--- SephiriaMod/Registries/ModItemExtensions.cs
+++ SephiriaMod/Registries/ModItemExtensions.cs
@@ -124,6 +124,16 @@ namespace SephiriaMod.Registries
             item.DamageId = ModDamageId.CreateAbility(id);
             return item;
         }
+        public static T SetDamageIdDebuff<T>(this T item, string id) where T : ModCharm
+        {
+            item.DamageId = ModDamageId.CreateDebuff(id);
+            return item;
+        }
+        public static T SetDamageIdDebuff<T>(this T item, string id, string key) where T : ModCharm
+        {
+            item.DamageId = ModDamageId.CreateDebuff(id, key);
+            return item;
+        }
         public static T SetStats<T>(this T item, params Charm_StatusInstance.StatusGroup[] stats) where T : ModCharmStatus
         {
             item.Stats = stats;
