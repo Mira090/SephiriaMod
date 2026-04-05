d63bb270d0f2c076fad3206278679ccabedd4a36
Mira090 <2935shu4182@gmail.com>
2026-04-05 12:46:31 +0900

    飛翔体の登録システムの準備

---
+++ SephiriaMod/Registries/ModMeleeCollision.cs
@@ -0,0 +1,27 @@
+﻿using SephiriaMod.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SephiriaMod.Registries
+{
+    public class ModMeleeCollision : ModProjectile
+    {
+
+        public string Name { get; internal set; } = "DaggerDashFx_Ice";
+        public string OriginalName { get; internal set; }
+        public void InitPrefab(MeleeCollision original)
+        {
+            var projectile = UnityEngine.Object.Instantiate(original);
+            projectile.gameObject.name = Name;
+            if(projectile is MeleeCollision_Circle circle)
+            {
+                circle.radius = 2f;
+            }
+
+
+            ResourcePrefab = projectile.gameObject;
+        }
+    }
+}
---
+++ SephiriaMod/Registries/ModProjectile.cs
@@ -0,0 +1,14 @@
+﻿using SephiriaMod.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SephiriaMod.Registries
+{
+    public class ModProjectile
+    {
+        public GameObject ResourcePrefab { get; internal set; }
+
+    }
+}
