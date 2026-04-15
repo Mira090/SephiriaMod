2ce32db510c007bc1e78bc45ce90e3b54959010e
Mira090 <2935shu4182@gmail.com>
2026-04-15 15:09:37 +0900

    宝飾アーティファクトを手に入れる奇跡を追加

---
+++ SephiriaMod/Miracles/Miracle_AddJewelry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SephiriaMod.Miracles
+{
+    [Obsolete]
+    public class Miracle_AddJewelry : Miracle_StatusInstance
+    {
+        protected override void SetOwnerInner(UnitAvatar owner)
+        {
+            base.SetOwnerInner(owner);
+            this.AddRandomJewelry();
+        }
+        protected override void DestroyInner()
+        {
+            base.DestroyInner();
+        }
+    }
+}
